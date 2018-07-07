5cdae95296a8c625c61987c61a2761979dcc942b
Veronica Bustabad <vbustabad@gmail.com>
2018-07-07 22:16:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ class App < Sinatra::Base
   end
 
   get '/multiply/:num1/:num2' do
-    @user_name = params[:name]
-    ":num1 * :num2"
+    @number1 = params[:num1]
+    @number2 = params[:num2]
+    "@number1 * @number2"
   end
 
 end
